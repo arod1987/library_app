2fc2059329e759dcd45c68c76205cddbe32ce236
Arunkumar Senthilnathan <arunkumar.senthilnathan@chronus.com>
2014-06-04 17:22:15 +0530

    Rename app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '74e31d3674839155d1904c97c6aa56cfddfca865032276238c09625555f06a606586254646c918a671f094990d8d11299830ca001f44b68656d3234a495acdd8'
+LibraryApp::Application.config.secret_token = '74e31d3674839155d1904c97c6aa56cfddfca865032276238c09625555f06a606586254646c918a671f094990d8d11299830ca001f44b68656d3234a495acdd8'
