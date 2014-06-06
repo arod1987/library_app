75b3f7d3d9ffd1e55a034f805d62c922088d8103
Arunkumar Senthilnathan <arunkumar.senthilnathan@chronus.com>
2014-06-06 13:19:57 +0530

    finish updating-users-part-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:show, :edit, :update]
-  before_filter :correct_user, only: [:show, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
   def show
     @user = User.find(params[:id])
   end
@@ -21,11 +23,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    #if @user.update_attributes(params[:user])
-      # Handle a successful update
-    #else
-    #  render 'edit'
-    #end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def update
@@ -38,14 +39,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   private
   
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
