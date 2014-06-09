3d729fbfdb507a4b587d86a95786729cdcce9ad4
Arunkumar Senthilnathan <arunkumar.senthilnathan@chronus.com>
2014-06-09 14:42:07 +0530

    Finish book edit, update, index, new and destroy actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,41 @@
 class BooksController < ApplicationController
   before_filter :signed_in_user, only: [:index]
-  before_filter :admin_user, only: [:edit, :update, :destroy]
+  before_filter :admin_user, only: [:new, :edit, :update, :destroy]
   
   def show
     @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def index
     @books = Book.paginate(page: params[:page])
+  end
+
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      flash[:success] = "New book added!"
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(params[:book])
+      flash[:success] = "Book updated"
+      redirect_to @book      
+    else
+      render 'edit'
+    end
   end
 
   def destroy
--- spec/requests/book_pages_spec.rb
+++ spec/requests/book_pages_spec.rb
@@ -4,13 +4,6 @@ describe "Book pages" do
   
   subject { page }
 
-  describe "addbook page" do
-    before { visit addbook_path }
-  
-    it { should have_selector('h1', text: 'Add new book') }
-    it { should have_selector('title', text: 'Add new book') }
-  end
-
   describe "book page" do
     let(:book) { FactoryGirl.create(:book) }
     before { visit book_path(book) }
@@ -53,15 +46,93 @@ describe "Book pages" do
       describe "as an admin user" do
         let(:admin) { FactoryGirl.create(:admin) }
         before do
-          sign_in admin
+          sign_in admin          
           visit books_path
         end
 
-        it { should have_link('delete') }
+        it { should have_link('delete', href: books_path) }
         it "should be able to delete book" do
           expect { click_link('delete') }.to change(Book, :count).by(-1)
         end
       end
+    end
+  end
+
+  describe "addbook" do
+  
+    let(:admin) { FactoryGirl.create(:admin) }
+
+    before do
+      sign_in admin
+      visit addbook_path
+    end
+
+    let(:submit) { "Add book" }
+
+    describe "with admin user" do
+      it {should have_selector('h1', text: 'Add new book') }
+      it {should have_selector('title', text: 'Add new book') }
+    end
+
+    describe "with invalid information" do
+      it "should not create a book" do
+        expect { click_button submit }.not_to change(Book, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Title", with: "LibraryBook"
+        fill_in "Author", with: "Author"
+        fill_in "Year", with: "2014"
+        fill_in "Synopsis", with: "This is a book"
+        fill_in "Copies", with: 5
+      end
+
+      it "should create a book" do
+        expect { click_button submit }.to change(Book, :count).by(1)
+      end
+    end
+  end
+
+  describe "edit" do
+    let(:admin) { FactoryGirl.create(:admin) }
+    let(:book) { FactoryGirl.create(:book) }
+    before do 
+      sign_in admin
+      visit edit_book_path(book)
+    end
+
+    describe "page" do
+      it { should have_selector('h1', text: "Update book") }
+      it { should have_selector('title', text: "Edit book") }
+    end
+
+    describe "with invalid information" do
+      before do
+        fill_in "Title", with: ""
+        click_button "Save changes"
+      end
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_title) { "New Library Book" }
+      let(:new_author) { "New Author" }
+      before do
+        fill_in "Title", with: new_title
+        fill_in "Author", with: new_author
+        fill_in "Synopsis", with: book.synopsis
+        fill_in "Year", with: book.year
+        fill_in "Copies", with: book.copies
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_title) }
+      it { should have_selector('div.alert.alert-success') }
+      specify { book.reload.title.should == new_title }
+      specify { book.reload.author.should == new_author }
     end
   end
 end
